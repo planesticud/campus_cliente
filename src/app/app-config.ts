f3ada20983c08447d686486ae6775aebf5ee6df2
jevilla94 <jvplanesticud@gmail.com>
2020-03-13 13:00:57 -0500

    Ajustes en cofig rutas planestic

--- src/app/app-config.ts
+++ src/app/app-config.ts
@@ -55,7 +55,7 @@ export const Config = {
     },
     CAMPUS_MID: 'https://autenticacion.portaloas.udistrital.edu.co/apioas/campus_mid/v1/',
     CORE_SERVICE: 'https://autenticacion.portaloas.udistrital.edu.co/apioas/core_crud/v1/',
-    DESCUENTO_ACADEMICO_SERVICE: 'https://autenticacion.portaloas.udistrital.edu.co/apioas/descuento_academico_crud/v1/',
+    DESCUENTO_ACADEMICO_SERVICE: 'https://autenticacion.portaloas.udistrital.edu.co/apioas/matriculas_descuentos_crud/v1/',
     // #revisar
     DOCENTE_SERVICE: 'https://autenticacion.portaloas.udistrital.edu.co/apioas/academica_jbpm/v1/',
     DOCUMENTO_SERVICE: 'https://autenticacion.portaloas.udistrital.edu.co/apioas/documento_crud/v1/',
@@ -75,6 +75,53 @@ export const Config = {
     PROGRAMA_OIKOS_SERVICE: 'https://autenticacion.portaloas.udistrital.edu.co/apioas/oikos_crud_api/v1/',
     SESIONES_SERVICE: 'https://autenticacion.portaloas.udistrital.edu.co/apioas/sesiones_crud/v1/',
     UBICACION_SERVICE: 'https://autenticacion.portaloas.udistrital.edu.co/apioas/ubicaciones_crud/v1/',
+    // #revisar
+    VALIDACION_OIKOS_SERVICE: 'https://autenticacion.portaloas.udistrital.edu.co/apioas/academica_jbpm/v1/',
+    // #revisar
+    VALIDACION_SNIES_SERVICE: 'https://autenticacion.portaloas.udistrital.edu.co/apioas/academica_jbpm/v1/',
+    CONFIGURACION_SERVICE: 'https://autenticacion.portaloas.udistrital.edu.co/apioas/configuracion_crud_api/v1/',
+    NOTIFICACION_SERVICE: 'ws://pruebasapi.intranetoas.udistrital.edu.co:8116/ws/join',
+    WSO2_SERVICE: 'http://jbpm.udistritaloas.edu.co:8280/services',
+    // #revisar
+    PAGO_SERVICE: 'http://prueba.campusvirtual.udistrital.edu.co/pagos/',
+    // #revisar
+    RECIBO_SERVICE: 'http://api.planestic.udistrital.edu.co:9017/v1/',
+    TOKEN: {
+      AUTORIZATION_URL: 'https://autenticacion.portaloas.udistrital.edu.co/oauth2/authorize',
+      CLIENTE_ID: '3r8DbyjNrVvcf5Z1CSsBGpGWzrwa',
+      RESPONSE_TYPE: 'id_token token',
+      SCOPE: 'openid email role documento',
+      REDIRECT_URL: 'https://cliente.campusvirtual.udistrital.edu.co/',
+      SIGN_OUT_URL: 'https://autenticacion.portaloas.udistrital.edu.co/oidc/logout',
+      SIGN_OUT_REDIRECT_URL: 'https://cliente.campusvirtual.udistrital.edu.co/',
+    },
+  },
+  PREPRODPL: {
+    NUXEO: {
+      PATH: 'https://documental.portaloas.udistrital.edu.co/nuxeo/',
+    },
+    CAMPUS_MID: 'http://api.planestic.udistrital.edu.co:8095/v1/',
+    CORE_SERVICE: 'http://api.planestic.udistrital.edu.co:8102/v1/',
+    DESCUENTO_ACADEMICO_SERVICE: 'http://api.planestic.udistrital.edu.co:9013/v1/',
+    // #revisar
+    DOCENTE_SERVICE: 'https://autenticacion.portaloas.udistrital.edu.co/apioas/academica_jbpm/v1/',
+    DOCUMENTO_SERVICE: 'http://api.planestic.udistrital.edu.co:8094/v1/',
+    DOCUMENTO_PROGRAMA_SERVICE: 'http://api.planestic.udistrital.edu.co:9014/v1/',
+    ENTE_SERVICE: 'http://api.planestic.udistrital.edu.co:8096/v1/',
+    EVALUACION_INSCRIPCION_SERVICE: 'http://api.planestic.udistrital.edu.co:9016/v1/',
+    EXPERIENCIA_SERVICE: 'http://api.planestic.udistrital.edu.co:8099/v1/',
+    FORMACION_ACADEMICA_SERVICE: 'http://api.planestic.udistrital.edu.co:8098/v1/',
+    FORMULARIO_SERVICE: 'http://api.planestic.udistrital.edu.co:9011/v1',
+    IDIOMA_SERVICE: 'http://api.planestic.udistrital.edu.co:8103/v1/',
+    INSCRIPCION_SERVICE: 'http://api.planestic.udistrital.edu.co:8887/v1/',
+    MENU_SERVICE: 'https://autenticacion.portaloas.udistrital.edu.co/apioas/configuracion_crud_api/v1/menu_opcion_padre/ArbolMenus/',
+    ORGANIZACION_SERVICE: 'http://api.planestic.udistrital.edu.co:8097/v1/',
+    PERSONA_SERVICE: 'http://api.planestic.udistrital.edu.co:8083/v1/',
+    PRODUCCION_ACADEMICA_SERVICE: 'http://api.planestic.udistrital.edu.co:9012/v1/',
+    PROGRAMA_ACADEMICO_SERVICE: 'http://api.planestic.udistrital.edu.co:8101/v1/',
+    PROGRAMA_OIKOS_SERVICE: 'https://autenticacion.portaloas.udistrital.edu.co/apioas/oikos_crud_api/v1/',
+    SESIONES_SERVICE: 'http://api.planestic.udistrital.edu.co:8081/v1/',
+    UBICACION_SERVICE: 'http://api.planestic.udistrital.edu.co:8085/v1/',
     // #revisar
     VALIDACION_OIKOS_SERVICE: 'https://autenticacion.portaloas.udistrital.edu.co/apioas/academica_jbpm/v1/',
     // #revisar
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -11,5 +11,6 @@
 export const environment = {
   production: false,
   // entorno: 'LOCAL',
-  entorno: 'PREPROD',
+  // entorno: 'PREPROD',
+  entorno: 'PREPRODPL',
 };
